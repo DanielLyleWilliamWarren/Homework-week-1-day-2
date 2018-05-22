4a98eec2e214e2c2705601ab76ece6acd14fa449
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-05-22 16:24:58 +0100

    first two tests passed

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,12 @@
+def return_10()
+  return 10
+end
+
+def add(first_number, second_number)
+ return (first_number + second_number)
+end
+
+# def test_add()
+#   add_result = add( 1, 2 )
+#   assert_equal( 3, add_result )
+# end
